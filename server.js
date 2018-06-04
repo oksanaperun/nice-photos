491cb2717e68c928b37de8e7804019d0e7715c6e
Oksana Perun <oksanaperun@mail.ru>
2018-06-05 00:30:59 +0300

    Trying to get working app on Heroku via node server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+// If an incoming request uses a protocol other than HTTPS,
+// redirect that request to the same url but with HTTPS
+const forceSSL = function() {
+    return function(req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+};
+// Instruct the app to use the forceSSL middleware
+app.use(forceSSL());
+
+// Actual host of the static Angular content
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 5000, function() {
+    console.log("Nice photos app running!");
+});
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
