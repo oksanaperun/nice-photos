9cae0846a612a050116c9220b1b8acc1c696d223
Oksana Perun <oksanaperun@mail.ru>
2018-06-09 22:02:12 +0300

    Prod environment setup

--- set-env.ts
+++ set-env.ts
@@ -1,13 +1,18 @@
 import { writeFile } from 'fs';
+import { argv } from 'yargs';
 
 // This is good for local dev environment, when it's better to
 // store a projects environment variables in a .gitignore'd file
 require('dotenv').config();
 
-const targetPath = './src/environments/environment.ts';
+const environment = argv.env;
+const isProd = environment === 'prod';
+const isProdFileName = isProd ? '.prod' : '';
+
+const targetPath = `./src/environments/environment${isProdFileName}.ts`;
 const envConfigFile = `
 export const environment = {
-  production: false,
+  production: ${isProd},
   apiBase: 'https://api.unsplash.com',
   clientId: '${process.env.CLIENT_ID}'
 };
